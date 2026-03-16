bde86caac16fdd528901ab5bf24fbd51d6180ba8
Manas <manaspaliwal275@gmail.com>
2026-03-16 19:46:04 +0530

    great learning experience.

--- flatning.py
+++ flatning.py
@@ -6,10 +6,11 @@ print('------------------------------------------------------version :- 1.2-----
 spark = SparkSession.builder \
     .appName("Flattening") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-#    .config("spark.hadoop.fs.s3a.access.key", "AKIAYUPL43FR4PJEDLNG") \
-#    .config("spark.hadoop.fs.s3a.secret.key", "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") \
-#    .config("spark.hadoop.fs.s3a.endpoint", "s3.eu-north-1.amazonaws.com") \
+    .config("spark.hadoop.fs.s3a.access.key", "AKIAYUPL43FR4PJEDLNG") \
+    .config("spark.hadoop.fs.s3a.secret.key", "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") \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.eu-north-1.amazonaws.com") \
     .getOrCreate()
+
 df1 = spark.read.option("multiline",'true').json("/opt/spark/work-dir/orders_etl.json")
 
 df3 = df1.select("*", explode("products").alias("product"))
@@ -20,8 +21,10 @@ print("----------------------------------DF4---------------------------")
 df4.printSchema();
 df4.show(truncate=False)
 
-df4.write.mode("overwrite").parquet("s3a://serverless-etl-project-manas/raw")
+# df4.write.mode("overwrite").parquet("s3a://serverless-etl-project-manas/raw")
 
+df1 = spark.read.parquet("s3a://serverless-etl-project-manas/raw");
+df1.show(truncate=False)
 
 print("-------------------------------------version 1.2 completed-----------------------------")
 
