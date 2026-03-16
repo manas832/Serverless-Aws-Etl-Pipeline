1f86c843498ab0e8df680baf56deda9ae1684aa0
Manas <manaspaliwal275@gmail.com>
2026-03-16 19:46:41 +0530

    great learning exp.

---
+++ transformations.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col 
+
+spark = SparkSession.builder \
+    .appName("transformations") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.access.key", "AKIAYUPL43FR4PJEDLNG") \
+    .config("spark.hadoop.fs.s3a.secret.key", "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") \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.eu-north-1.amazonaws.com") \
+    .getOrCreate()
+
+df1 = spark.read.parquet("s3a://serverless-etl-project-manas/raw")
+print('-------------------------------------------customers_df--------------------------------------')
+customers_df = df1.select( col('customer_id'), col('name').alias('customer_name'), col('email').alias('customer_email'), col('address').alias('customer_address') )
+customers_df.show()
+customers_df.write.parquet("s3a://serverless-etl-project-manas/processed/customers_df", mode="overwrite");
+print("-------------------------------------------orders_df-------------------------------------- ")
+orders_df = df1.select(col('order_id'), col('customer_id'), col('order_date'), col('total_amount'))
+orders_df.show()
+orders_df.write.parquet("s3a://serverless-etl-project-manas/processed/orders_df", mode="overwrite");
+print("-------------------------------------------products_df-------------------------------------- ")
+products_df = df1.select('product.product_id', col('product.name').alias('product_name'), col('product.category').alias('product_category'), col('product.price').alias('product_price') , col('product.quantity').alias('product_quantity'), col('order_id'), col('customer_id'))
+products_df.show()
+products_df.write.parquet("s3a://serverless-etl-project-manas/processed/products_df", mode="overwrite");
+
+spark.stop();
